18c763854fa68443600ad5252a5cdfd95f6feac0
weeecape <dzmitry.mikhasko@gmail.com>
2025-02-03 08:45:30 +0300

    ignore config

--- config.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = '8198537389:AAFtt5NuVEzQ-ATXiGG162lh7Ox6aDsTiqE'
-weather_api = '41e8d4859f610037ea5cfd8742b847f1'
-open_weather_url = (
-    "https://api.openweathermap.org/data/2.5/weather?"
-    "lat={latitude}&lon={longitude}&"
-    "appid=" + weather_api + "&lang=ru&"
-    "units=metric"    
-)
